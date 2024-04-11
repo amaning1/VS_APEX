8fad3a3383a55c9eb6aadf490d59093a6c6f8f9a
kelvin.mills <amaningmills@gmail.com>
2024-04-11 15:50:04 +0000

    changes made

---
+++ force-app/main/default/lwc/accountContacts/accountContacts.js
@@ -0,0 +1,65 @@
+import { LightningElement,api,wire } from 'lwc';
+
+import {
+    subscribe,
+    unsubscribe,
+    APPLICATION_SCOPE,
+    MessageContext,
+} from 'lightning/messageService';
+import viewAccountContactsChannel from '@salesforce/messageChannel/viewAccountContactsChannel__c';
+import getAccountContacts from '@salesforce/apex/LWCGetAccountsController.getAccountContacts';
+
+export default class AccountContacts extends LightningElement {
+    subscription=null;
+    accountId='';
+    @wire(MessageContext)
+    messageContext;
+    title='Contacts';
+    contacts = [];
+
+    async getContacts(){
+        const data = await getAccountContacts({accountId: this.accountId});
+        this.contacts = data;
+    }
+
+    get isAccountSelected(){
+        return this.accountId?true:false;
+    }
+
+    get hasContacts(){
+        return this.contacts?.length > 0;
+    }
+
+    // Encapsulate logic for Lightning message service subscribe and unsubsubscribe
+    subscribeToMessageChannel() {
+        if (!this.subscription) {
+            this.subscription = subscribe(
+                this.messageContext,
+                viewAccountContactsChannel,
+                (data) => this.handleAccountSelection(data),
+                { scope: APPLICATION_SCOPE }
+            );
+        }
+    }
+
+    unsubscribeToMessageChannel() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+
+    // Handler for message received by component
+    handleAccountSelection(data) {
+        this.accountId = data.accountId;
+        this.title = ' ${data.accountName}\'s Contacts'
+    }
+
+    // Standard lifecycle hooks used to subscribe and unsubsubscribe to the message channel
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+
+    disconnectedCallback() {
+        this.unsubscribeToMessageChannel();
+    }
+
+}
--- force-app/main/default/lwc/accountSearchResults/accountSearchResults.js
+++ force-app/main/default/lwc/accountSearchResults/accountSearchResults.js
@@ -1,6 +1,9 @@
 import { LightningElement,api, wire} from 'lwc';
 import getAccounts  from '@salesforce/apex/LWCGetAccountsController.getAccounts';
 
+import { publish, MessageContext } from 'lightning/messageService';
+import viewAccountContactsChannel from '@salesforce/messageChannel/viewAccountContactsChannel__c';
+
 
 const COLUMNS = [
     { label: 'Id', fieldName: 'Id' },
@@ -21,5 +24,17 @@ export default class AccountSearchResults extends LightningElement {
     columns=COLUMNS;
     @wire(getAccounts,{searchText: '$searchText'})
     accountsList;
+
+    @wire(MessageContext)
+    messageContext;
+
+    rowActionHandler(event) {
+        if(event.detail.action.value == 'View_Contacts'){
+            const payload = { accountId: event.detail.row.Id, accountName: event.detail.row.Name };
+
+            publish(this.messageContext, recordSelected, payload);
+        }
+       
+    }
 
 } 
